5890583ece5afd327a4ff705df5f3a48c9c5de49
cordellbonnieux <cordellbonnieux@gmail.com>
2020-08-17 20:02:29 -0700

    The form now totally works as intended! Now on to styling the project.

--- script-alt.js
+++ script-alt.js
@@ -7,7 +7,6 @@ function Book(title, author, year, pages, read){
     this.year = year,
     this.pages = pages,
     this.read = read
-    //might need to add the printing ability here?
 }
 // Shows Dialog Boxes
 function showDialog(){
@@ -21,24 +20,35 @@ function hideDialog(){
     document.getElementById('dialogBox').style.display = "none";
     return
 } 
-// addBook
+// Add Book button in dialog box
 let addBookButton = document.getElementById('addBook');
 addBookButton.addEventListener('click', function(){
     let title = document.getElementById('bookTitle').value;
     let author = document.getElementById('authorName').value;
     let year = Number(document.getElementById('yearPublished').value);
     let pages = Number(document.getElementById('numberOfPages').value);
-    let read = document.getElementById('haveRead').value;
+    let read = document.getElementById('haveRead').checked;
+    if (read == true){
+        read = "Have read";
+    } else {
+        read = "Have not read";
+    }
     myLibrary.push(new Book(title, author, year, pages, read));
+    libraryRefresh();
+    title.value = "", author.value = "", year.value = "", pages.value = "", read.value = false;
     return hideDialog();
 });
-
+// Refreshes and prints all books to the Library
+function libraryRefresh(){
+    libraryGrid.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++){
+        libraryGrid.innerHTML += "<div class='bookWrapper' style='display:inline-block;'> <h2>" + myLibrary[i]["title"] + "</h2><h3>by " + myLibrary[i]["author"] + "</h3><p>Published in " + myLibrary[i]["year"] + "</p><p>" + myLibrary[i]["pages"] + " pages</p><p>" + myLibrary[i]["read"] + "</p></div>";
+    }
+    return;
+}
 
 
 
 myLibrary.push(new Book("The Fellowship of the Ring","J.R.R. Tolkien",1954,423,true));
-// this works
-//for (let i = 0; i < myLibrary.length; i++){
-//    libraryGrid.innerHTML += "<div class='bookWrapper' style='display:inline-block;'><ul><li>" + myLibrary[i]["title"] + "</li><li>" + myLibrary[i]["author"] + "</li><li>" + myLibrary[i]["year"] + "</li><li>" + myLibrary[i]["pages"] + "</li><li>" + myLibrary[i]["read"] + "</li></div>";
-//}
+
 
