e4a0453323ad4e79c5b9e5d0866decaf76ec9186
cordellbonnieux <cordellbonnieux@gmail.com>
2020-08-17 19:24:33 -0700

    Finally got the event listener working for the form. The answer was simplier then I thought and I think I just needed to step back! Onto completing the project.

--- script-alt.js
+++ script-alt.js
@@ -1,22 +1,7 @@
 let myLibrary = [];
 let libraryGrid = document.getElementById('libraryGrid');
-// when 'add book' is clicked
-function addBook(){
-    let bookTitle = document.getElementById('bookTitle').value;
-    let authorName = document.getElementById('authorName').value;
-    let yearPublished = document.getElementById('yearPublished').value;
-    let numberOfPages = document.getElementById('numberOfPages').value;
-    let haveRead = document.getElementById('haveRead').value;
-        //check for checked or not.
-    myLibrary.push(new book(bookTitle, authorName, yearPublished, numberOfPages, haveRead));
-    // now store these values on the page
-    for (let i = 0; i < myLibrary.length; i++){
-        libraryGrid.innerHTML += "<div class='bookWrapper'><ul><li>" + myLibrary[i]["title"] + "</li><li>" + myLibrary[i]["author"] + "</li><li>" + myLibrary[i]["year"] + "</li><li>" + myLibrary[i]["pages"] + "</li><li>" + myLibrary[i]["read"] + "</li></div>";
-    }
-    return
-}
 // Creates a book
-function book(title, author, year, pages, read){
+function Book(title, author, year, pages, read){
     this.title = title,
     this.author = author,
     this.year = year,
@@ -30,10 +15,30 @@ function showDialog(){
     document.getElementById('dialogBox').style.display = "block";
     return
 }
+// Hides Dialog Boxes
+function hideDialog(){
+    document.getElementById('dialogBackground').style.display = "none";
+    document.getElementById('dialogBox').style.display = "none";
+    return
+} 
+// addBook
+let addBookButton = document.getElementById('addBook');
+addBookButton.addEventListener('click', function(){
+    let title = document.getElementById('bookTitle').value;
+    let author = document.getElementById('authorName').value;
+    let year = Number(document.getElementById('yearPublished').value);
+    let pages = Number(document.getElementById('numberOfPages').value);
+    let read = document.getElementById('haveRead').value;
+    myLibrary.push(new Book(title, author, year, pages, read));
+    return hideDialog();
+});
 
 
-myLibrary.push(new book("The Fellowship of the Ring","J.R.R. Tolkien",1954,423,true));
 
-for (let i = 0; i < myLibrary.length; i++){
-    libraryGrid.innerHTML += "<div class='bookWrapper' style='display:inline-block;'><ul><li>" + myLibrary[i]["title"] + "</li><li>" + myLibrary[i]["author"] + "</li><li>" + myLibrary[i]["year"] + "</li><li>" + myLibrary[i]["pages"] + "</li><li>" + myLibrary[i]["read"] + "</li></div>";
-}
+
+myLibrary.push(new Book("The Fellowship of the Ring","J.R.R. Tolkien",1954,423,true));
+// this works
+//for (let i = 0; i < myLibrary.length; i++){
+//    libraryGrid.innerHTML += "<div class='bookWrapper' style='display:inline-block;'><ul><li>" + myLibrary[i]["title"] + "</li><li>" + myLibrary[i]["author"] + "</li><li>" + myLibrary[i]["year"] + "</li><li>" + myLibrary[i]["pages"] + "</li><li>" + myLibrary[i]["read"] + "</li></div>";
+//}
+
